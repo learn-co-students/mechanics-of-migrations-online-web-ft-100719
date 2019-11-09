1c9685900ff6daad165ed06f48708e3cec97d6ff
Huda Yousif <hjoski@gmail.com>
2019-11-09 15:09:09 -0800

    Done.

--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -1,5 +1,6 @@
 require_relative './spec_helper'
 
+
 describe "Artist" do
   let(:artist) {Artist.new(name: 'Justice', genre: 'Electronic', age: Time.now.strftime("%Y").to_i - 2003, hometown: "Paris")}
 
